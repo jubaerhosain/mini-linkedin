20e83836592ba3c6e6dae032b9723b53060fde58
JubaerHosain <jubaerhosain1119@gmail.com>
2023-07-18 11:51:05 +0600

    updated

--- monolithic/back-end/middlewares/common/defaultErrorHandler.js
+++ monolithic/back-end/middlewares/common/defaultErrorHandler.js
@@ -1,5 +1,6 @@
 import { Response } from "../../utils/responseUtils.js";
 
 export default function defaultErrorHandler(err, req, res, next) {
+    console.log(err);
     res.status(500).json(Response.error("Internal Server Error", Response.SERVER_ERROR));
 }
