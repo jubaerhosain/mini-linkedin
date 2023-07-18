2c55419777685fc63100a5db970804e267e69fcc
JubaerHosain <jubaerhosain1119@gmail.com>
2023-07-18 19:25:39 +0600

    updated

--- monolithic/back-end/jobs/jobs.js
+++ monolithic/back-end/jobs/jobs.js
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import notificationRepository from "../repositories/notificationRepository.js";
 
 function startDeleteNotificationJob() {
-    cron.schedule("*/1 * * * *", notificationRepository.deleteOldNotifications);
+    cron.schedule("*/2 * * * *", notificationRepository.deleteOldNotifications);
     console.log("Delete old notifications job started...");
 }
 
