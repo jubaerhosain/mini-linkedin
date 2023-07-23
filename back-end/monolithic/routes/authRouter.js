58ea1a88722016eb0636670df86dce8931243503
JubaerHosain <jubaerhosain1119@gmail.com>
2023-07-24 00:20:24 +0600

    updated

--- back-end/monolithic/routes/authRouter.js
+++ back-end/monolithic/routes/authRouter.js
@@ -5,12 +5,12 @@ import authValidator from "../middlewares/validators/authValidator.js";
 import authController from "../controllers/authController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
-authRouter.post("/register", authValidator.registrationValidator, authController.register)
+authRouter.post("/register", authValidator.registrationValidator, authController.register);
 
-authRouter.post("/login", authValidator.loginValidator, authController.login)
+authRouter.post("/login", authValidator.loginValidator, authController.login);
 
-authRouter.delete("/logout", authController.logout)
+authRouter.delete("/logout", authController.logout);
 
-authRouter.get("/", authMiddleware.checkAuthentication, authController.getLoggedInUser)
+authRouter.get("/", authMiddleware.checkAuthentication, authController.getLoggedInUser);
 
 export default authRouter;
