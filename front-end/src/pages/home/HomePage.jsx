d568a476f607cd77481df3f100817290d6770714
JubaerHosain <jubaerhosain1119@gmail.com>
2023-07-26 13:59:50 +0600

    updated

--- front-end/src/pages/home/HomePage.jsx
+++ front-end/src/pages/home/HomePage.jsx
@@ -1,38 +1,37 @@
-const postsData = [
-  {
-    id: 1,
-    user: {
-      name: "John Doe",
-      image: "/blank_person.png",
-    },
-    text: "Hello, this is my first post!",
-    image: "/blank_person.png",
-  },
-  {
-    id: 2,
-    user: {
-      name: "Jane Smith",
-      image: "/blank_person.png",
-    },
-    text: "Excited to share my latest adventure!",
-    image: "/logo.png",
-  },
-  // Add more posts here
-];
+import { useEffect, useState } from "react";
+import axiosInstance from "../../config/axiosInstance";
 
 export default function HomePage() {
+  const [postsData, setPostData] = useState([]);
+
+  useEffect(() => {
+    const getPostData = async () => {
+      try {
+        const response = await axiosInstance.get("/post");
+        console.log(response.data.data);
+        setPostData(response.data.data);
+      } catch (err) {
+        console.log(err.response?.data);
+      }
+    };
+    getPostData();
+  }, []);
+
   return (
     <div className="container mx-auto py-6 max-w-xl">
-      {postsData.map((post) => (
-        <div key={post.id} className="post bg-white shadow-md rounded-md p-4 mb-4">
-          <div className="flex items-center mb-4">
-            <img src={post.user.image} alt={post.user.name} className="w-10 h-10 rounded-full mr-2" />
-            <span className="font-bold">{post.user.name}</span>
+      {postsData.map((post) => {
+        return (
+          <div key={post._id} className="post bg-white shadow-md rounded-md p-4 mb-4">
+            <div className="flex items-center mb-4">
+              <img src={"/blank_person.png"} alt={"Image"} className="w-10 h-10 rounded-full mr-2" />
+              <span className="font-bold">{"Name"}</span>
+            </div>
+            <p className="text-gray-700 mb-2">{post.content}</p>
+            {/* {post.img_url && <img src={post.img_url} alt="Post" className="w-full rounded-md" />} */}
+            {post.img_url && <img src={post.img_url} alt="Post" className="w-full rounded-md" />}
           </div>
-          <p className="text-gray-700 mb-2">{post.text}</p>
-          {post.image && <img src={post.image} alt="Post" className="w-full rounded-md" />}
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
