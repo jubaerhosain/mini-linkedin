78af2b5f056902e53868f6c2567d5f9dc469bb06
JubaerHosain <jubaerhosain1119@gmail.com>
2023-07-26 14:09:40 +0600

    updated

--- front-end/src/pages/notification/NotificationPage.jsx
+++ front-end/src/pages/notification/NotificationPage.jsx
@@ -1,29 +1,21 @@
-import { useState } from "react";
-
-const notificationsData = [
-  {
-    id: 1,
-    user: {
-      name: "John Doe",
-      image: "/blank_person.png",
-    },
-    message: "You have a new friend request.",
-    is_read: false,
-  },
-  {
-    id: 2,
-    user: {
-      name: "Jane Smith",
-      image: "/blank_person.png",
-    },
-    message: "Your post was liked by someone.",
-    is_read: true,
-  },
-  // Add more notifications here
-];
+import { useEffect, useState } from "react";
+import axiosInstance from "../../config/axiosInstance";
 
 const NotificationsPage = () => {
-  const [notifications, setNotifications] = useState(notificationsData);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const getNotifications = async () => {
+      try {
+        const response = await axiosInstance.get("notification");
+        console.log(response.data);
+        setNotifications(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNotifications();
+  }, []);
 
   const markAllAsRead = () => {
     const updatedNotifications = notifications.map((notification) => ({
@@ -45,18 +37,17 @@ const NotificationsPage = () => {
       </div>
       {notifications.map((notification) => (
         <div
-          key={notification.id}
-          className={` flex flex-row justify-between notification ${
-            notification.is_read ? "" : "bg-gray-300"
+          key={notification._id}
+          className={` flex flex-row justify-between notification p-3 my-2 ${
+            notification.is_read ? "bg-gray-200" : "bg-gray-300"
           }`}
         >
-          <img
+          {/* <img
             src={notification.user.image}
             alt={notification.user.name}
             className="w-10 h-10 rounded-full mr-4"
-          />
+          /> */}
           <div>
-            <p className="font-bold">{notification.user.name}</p>
             <p className="text-gray-700">{notification.message}</p>
           </div>
         </div>
